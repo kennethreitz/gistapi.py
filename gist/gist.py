926f341dc05fff7b0398fb630b6747c7cc6ac555
Kenneth Reitz <me@kennethreitz.com>
2010-05-16 10:19:49 -0400

    lazy api calls working. freaking awesome.

--- gist/gist.py
+++ gist/gist.py
@@ -11,12 +11,12 @@ from cStringIO import StringIO
 
 __version__ = "$Revision: 68852 $"
 
+
 class Gists(object):
 	"""Gist API wrapper"""
 	def __init__(self, username=None, token=None):
 		# raise NotImplementedError
 		self._username = username; self._token = token
-		
 		
 	def __repr__(self):
 		pass
@@ -33,19 +33,34 @@ class Gist(object):
 		self.id = id
 		self.url = 'http://github.com/{0}'.format(id)
 		self.embed_url = 'http://github.com/{0}.js'.format(id)
-		self._get_meta()
+	
+	def __getattribute__(self, name):
+		"""Gets attributes, but only if needed"""
 
+		# Only make API call if needed
+		if name in ['description', 'created_at', 'public', 'files', 'repo']: 
+			if not hasattr(self, '_meta'): self._meta = self._get_meta
+			
+		return object.__getattribute__(self, name)
+
+	@property
 	def _get_meta(self):
+		'''Fetches Gist Metadata'''
+		
 		_meta_url = 'http://gist.github.com/api/v1/json/{0}'.format(self.id)
 		_meta = simplejson.load(urllib.urlopen(_meta_url))['gists'][0]
 		
+		# Get all response properties
 		[setattr(self, attr, _meta[attr]) for attr in _meta.iterkeys()]
-	
-	
-	
+
+		return True
+
+
 if __name__ == '__main__':
 	
 	gist = Gist(347430)
 	
+	# print gist.id
+	print gist.description
 	print gist.files
 	
